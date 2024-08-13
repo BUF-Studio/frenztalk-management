7406e014b2c88ce7bef3c36e01be887a7bb1b0ea
zelton02 <zhiheng426@gmail.com>
2024-08-13 22:12:48 +0800

    Done academic setup for subject

---
+++ app/back/academic-setup/layout.tsx
@@ -0,0 +1,30 @@
+export default function Layout({
+    children,
+    subjectList,
+    pricing,
+    educationLevel,
+  }: Readonly<{
+    children: React.ReactNode;
+    subjectList: React.ReactNode;
+    pricing: React.ReactNode;
+    educationLevel: React.ReactNode;
+  }>) {
+    return (
+      <div className="flex h-full gap-4">
+        <div className="flex flex-1">
+          {subjectList}
+        </div>
+  
+        <div className="flex flex-1 flex-col gap-4 h-full">
+          <div className="flex flex-1 min-h-0">
+            {educationLevel}
+          </div>
+  {/* 
+          <div className="flex flex-1 min-h-0">
+            {pricing}
+          </div> */}
+        </div>
+      </div>
+    );
+  }
+  
