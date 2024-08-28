05d72894a4bd12cd0f2f927cea9577297530969d
zelton02 <zhiheng426@gmail.com>
2024-08-28 21:56:44 +0800

    feat: modify middleware to redirect non-verified user

--- lib/firebase/service/serverApp.ts
+++ lib/firebase/service/serverApp.ts
@@ -1,26 +1,19 @@
-// enforces that this code can only be called on the server
-// https://nextjs.org/docs/app/building-your-application/rendering/composition-patterns#keeping-server-only-code-out-of-the-client-environment
-import "server-only";
-
 import * as admin from "firebase-admin";
-import { initializeApp, getApps } from "firebase-admin/app";
-import { getAuth } from "firebase-admin/auth";
-import { firebaseAdminConfig } from "./config";
 
 const serviceAccount = require("@/frenztalk-firebase-adminsdk-h68uw-8c7b585859.json");
 
-export const firebaseAdminApp =
-  getApps().length === 0
-    ? initializeApp({
-        credential: admin.credential.cert(
-          // {
-          //   projectId: firebaseAdminConfig.projectId,
-          //   clientEmail: firebaseAdminConfig.clientEmail,
-          //   privateKey: firebaseAdminConfig.privateKey?.replace(/\\n/g, '\n')
-          // }
-          serviceAccount
-        ),
-      })
-    : getApps()[0];
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(
+      //   {
+      //   projectId: process.env.FIREBASE_PROJECT_ID,
+      //   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      //   privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      // }
+      serviceAccount
+    ),
+  });
+}
 
-export const auth = getAuth(firebaseAdminApp);
+export const auth = admin.auth();
+export const db = admin.firestore();
