b10f854bbfdbd2cfd77ed81d91672816a9a75f8d
KyangTan <kwanyang29@gmail.com>
2024-09-02 17:09:51 +0800

    refactor: manually updated the tabs.tsx to the latest version (previously merged with conflict)

--- app/components/general/tabs.tsx
+++ app/components/general/tabs.tsx
@@ -4,8 +4,6 @@ import type React from "react";
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { cn } from "@/utils/manage-class-name";
-import { SearchBar } from "./input/searchBar";
-import { useSearchTableData } from "@/lib/general_hooks/useSearchTableData";
 
 type Tab = {
   title: string;
@@ -21,8 +19,6 @@ export const Tabs = ({
   tabClassName,
   contentClassName,
   notificationClassName,
-  searchTerm,
-  setSearchTerm,
 }: {
   tabs: Tab[];
   containerClassName?: string;
@@ -30,8 +26,6 @@ export const Tabs = ({
   tabClassName?: string;
   contentClassName?: string;
   notificationClassName?: string;
-  searchTerm: string;
-  setSearchTerm: React.Dispatch<React.SetStateAction<string>>;
 }) => {
   const [active, setActive] = useState<Tab>(propTabs[0]);
   const [tabs, setTabs] = useState<Tab[]>(propTabs);
@@ -46,7 +40,6 @@ export const Tabs = ({
 
   const [hovering, setHovering] = useState(false);
 
-
   //TODO: Optimize this useEffect : kyang
   useEffect(() => {
     setTabs(propTabs);
@@ -60,52 +53,50 @@ export const Tabs = ({
     <div className="w-full">
       <div
         className={cn(
-          "flex flex-row items-center justify-between [perspective:1000px] relative overflow-auto sm:overflow-visible no-visible-scrollbar max-w-full w-full",
+          "flex flex-row items-center justify-start [perspective:1000px] relative overflow-auto sm:overflow-visible no-visible-scrollbar max-w-full w-full",
           containerClassName
         )}
       >
-        <div className="flex flex-row items-center space-x-4">
-          {propTabs.map((tab, idx) => (
-            <button
-              type="button"
-              key={tab.title}
-              onClick={() => {
-                moveSelectedTabToTop(idx);
-              }}
-              onMouseEnter={() => setHovering(true)}
-              onMouseLeave={() => setHovering(false)}
-              className={cn(
-                "relative flex flex-row px-4 py-2 rounded-full",
-                tabClassName
-              )}
-              style={{
-                transformStyle: "preserve-3d",
-              }}
-            >
-              {active.value === tab.value && (
-                <motion.div
-                  layoutId="clickedbutton"
-                  transition={{ type: "spring", bounce: 0.3, duration: 0.6 }}
-                  className={cn(
-                    "absolute inset-0 bg-gray-200 dark:bg-zinc-800 rounded-full ",
-                    activeTabClassName
-                  )}
-                />
-              )}
+        {propTabs.map((tab, idx) => (
+          <button
+            type="button"
+            key={tab.title}
+            onClick={() => {
+              moveSelectedTabToTop(idx);
+            }}
+            onMouseEnter={() => setHovering(true)}
+            onMouseLeave={() => setHovering(false)}
+            className={cn(
+              "relative flex flex-row px-4 py-2 rounded-full",
+              tabClassName
+            )}
+            style={{
+              transformStyle: "preserve-3d",
+            }}
+          >
+            {active.value === tab.value && (
+              <motion.div
+                layoutId="clickedbutton"
+                transition={{ type: "spring", bounce: 0.3, duration: 0.6 }}
+                className={cn(
+                  "absolute inset-0 bg-gray-200 dark:bg-zinc-800 rounded-full ",
+                  activeTabClassName
+                )}
+              />
+            )}
 
             <span className="relative block text-black dark:text-white">
               {tab.title}
             </span>
-            {tab.notify !== undefined && tab.notify > 0 && (
+            {tab.notify && tab.notify > 0 && (
               <span
                 className={cn(
                   "z-10 bg-red-500 text-xs font-medium text-white",
                   "flex items-center justify-center",
-                  "w-5 h-5 rounded-full ml-2",
+                  "w-5 h-5 rounded-full",
                   active.value === tab.value
                     ? "absolute -top-1 -right-1"
-                    : "relative",
-                  notificationClassName
+                    : "relative ml-2"
                 )}
               >
                 {tab.notify}
