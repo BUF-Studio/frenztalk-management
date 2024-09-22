100a15fe3c85084d6e07c2393d6ad84087201f09
zelton02 <zhiheng426@gmail.com>
2024-09-23 00:32:37 +0800

    fix: Clean invoice page

--- app/(main)/invoices/page.tsx
+++ app/(main)/invoices/page.tsx
@@ -1,43 +1,16 @@
 "use client";
 
-import { useInvoicePage } from "@/lib/context/page/invoicePageContext";
-import type { Invoice } from "@/lib/models/invoice";
-import { Plus } from "lucide-react";
-import { useRouter } from "next/navigation";
-import { Badge, type BadgeProps } from "@/app/components/general/badge";
 import { useStudents } from "@/lib/context/collection/studentsContext";
-import { capitalizeFirstLetter, formatDateRange } from "@/utils/util";
-import { usePayments } from "@/lib/context/collection/paymentContext";
-import { useState } from "react";
-import { useTableColumn } from "@/lib/general_hooks/useTableColumn";
-import { TableOrderEnum } from "@/lib/enums/TableOrderEnum";
-import { SearchBar } from "@/app/components/general/input/searchBar";
-import { useSearchTableData } from "@/lib/general_hooks/useSearchTableData";
-import InvoiceModalDialog from "./components/invoiceModalDialog";
-import { useInvoices } from "@/lib/context/collection/invoiceContext";
 import { useTutors } from "@/lib/context/collection/tutorContext";
 import { DataTable } from "@/app/components/ui/data-table";
 import { columns } from "./columns";
 import { useMergeInvoices } from "@/lib/context/collection/mergeInvoiceContext";
+import { useRouter } from "next/navigation";
 
 export default function InvoiceList() {
-  const { invoices } = useInvoices();
-  const { payments } = usePayments();
   const router = useRouter();
-  // const { invoice, setInvoice } = useInvoicePage();
+  const { setInvoice } = useMergeInvoices();
   const { mergeInvoices } = useMergeInvoices();
-  const { students } = useStudents();
-  const { tutors } = useTutors();
-
-  const findStudent = (id: string) => {
-    const student = students.find((student) => student.id === id);
-    return student;
-  };
-
-  const findTutor = (id: string) => {
-    const tutor = tutors.find((tutor) => tutor.id === id);
-    return tutor;
-  };
 
   return (
     <div>
@@ -47,10 +20,10 @@ export default function InvoiceList() {
       <DataTable
         data={mergeInvoices}
         columns={columns}
-        // getRowHref={(invoice) => {
-        //   router.push(`/invoices/${invoice.id}`);
-        //   setInvoice(invoice);
-        // }}
+        getRowHref={(invoice) => {
+          router.push(`/invoices/${invoice.id}`);
+          setInvoice(invoice);
+        }}
       />
     </div>
   );
